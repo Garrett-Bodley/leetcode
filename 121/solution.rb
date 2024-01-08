cd66ba4a42465a1a9c73102dfacb9a05a32314f6
Garrett Bodley <garrett.bodley@gmail.com>
2024-01-08 17:19:50 -0500

    Ruby solution 121.

---
+++ 121/solution.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+require 'pry-nav'
+
+def max_profit(p)
+  min = Float::INFINITY
+  profit = -Float::INFINITY
+  p.each do |val|
+    min = [min, val].min
+    profit = [profit, val - min].max
+  end
+  profit
+end
+
+input = [7, 1, 5, 3, 6, 4]
+
+puts max_profit(input)
