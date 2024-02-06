4d7827a12995dfac85e085f70466e9c42ed1173b
Garrett Bodley <garrett.bodley@gmail.com>
2024-02-06 16:42:05 -0500

    Ruby Solution #155.

---
+++ 0155/0155.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/min-stack/
+
+class MinStack
+  def initialize
+    @stack = []
+    @min_stack = []
+  end
+
+=begin
+  :type val: Integer
+  :rtype: Void
+=end
+  def push(val)
+    @min_stack << val if @min_stack.empty? || val <= @min_stack.last
+    @stack << val
+  end
+
+=begin
+  :rtype: Void
+=end
+  def pop()
+    removed = @stack.pop
+    @min_stack.pop if @min_stack.last == removed
+    removed
+  end
+
+=begin
+  :rtype: Integer
+=end
+  def top()
+    @stack.last
+  end
+
+=begin
+  :rtype: Integer
+=end
+  def get_min()
+    @min_stack.last
+  end
+end
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack.new()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.get_min()
