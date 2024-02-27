f3068c1b814175bc61ba4dd66351fe9a67979b0a
Garrett Bodley <garrett.bodley@gmail.com>
2024-02-27 16:26:36 -0500

    Alternate Ruby solution to #543.

--- 0543/0543.rb
+++ 0543/0543.rb
@@ -15,3 +15,20 @@ def traverse(node, diam = 0)
   diam = [d1, d2, l + r + 1].max
   [h + 1, diam]
 end
+
+# I think this is more readable tbh
+
+def diameter_of_binary_tree(root)
+  max = [0]
+  max_depth(root, max)
+  max[0]
+end
+
+def max_depth(node, max)
+  return 0 if node.nil?
+
+  l = max_depth(node.left, max)
+  r = max_depth(node.right, max)
+  max[0] = [max[0], l + r].max
+  [l, r].max + 1
+end
