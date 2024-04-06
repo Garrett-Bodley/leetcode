01738454210da586449399d6128e6b3865ce09f6
Garrett Bodley <garrett.bodley@gmail.com>
2024-04-06 17:48:27 -0400

    Commit unarchived changes for #201.

--- 0201/0201.rb
+++ 0201/0201.rb
@@ -2,9 +2,10 @@
 
 # https://leetcode.com/problems/bitwise-and-of-numbers-range
 
+require 'pry-nav'
+
 def range_bitwise_and(left, right)
   return 0 if left.zero?
-
   count = 0
   while left != right
     left >>= 1
@@ -12,4 +13,22 @@ def range_bitwise_and(left, right)
     count += 1
   end
   right << count
+end
+
+# Not sure what I was doing on these two tbh
+def range_bitwise_or(left, right)
+  res = msb_set = Math.log2(left).floor
+  mask = 1
+  while mask < msb_set
+    res |= mask
+    mask << 1
+  end
+  res
+end
+
+def brute_force(left, right)
+  return right if left.zero?
+  return left if left == right
+
+  (left..right).to_a.reduce(0) { _1 | _2 }
 end
